f573337bc2f917a0b327a947c927c5115c843888
Zachary T Cross <zachary.t.cross@gmail.com>
2014-11-19 20:14:56 -0700

    Implemented functions for assignment 2
    Implemented makeCacheMatrix and cacheSolve, as required for Programming
    Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The two functions below allow a user to speed up the
+# calculation of a matrix's inverse, by caching the result
+# of the solve() function.  This means that the actual
+# call to solve() only has to be performed once, as long
+# as the matrix in question is not changed.
 
-## Write a short comment describing this function
 
+# makeCacheMatrix - convert the matrix 'x' into a cache-able matrix
 makeCacheMatrix <- function(x = matrix()) {
+    cached_solution <- NULL
 
+    # This function is used to 'set' the value of the original matrix
+    set <- function(y){
+        x <<- y
+        cached_solution <<- NULL
+    }
+
+    # This function returns the original matrix
+    get <- function() x
+
+    # This function is used internally to store the results of solve()
+    set_cache <- function(inverse_matrix) cached_solution <<- inverse_matrix
+
+    # This function allows cacheSolve() to use the cached inverse matrix
+    get_cache <- function() cached_solution
+
+    # Here we return a list that contains the functions needed to operate
+    # our 'special' cache-able matrix.
+    list(set = set,
+         get = get,
+         set_cache = set_cache,
+         get_cache = get_cache)
 }
 
 
-## Write a short comment describing this function
-
+# Solve a matrix created via makeCacheMatrix.  Returns a cached solution if possible.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cached_solution <- x$get_cache()  # Get the currently-stored cache value
+
+    if(!is.null(cached_solution)) {  # If the cache value is not null, return it
+        message("getting cached solution")
+        return(cached_solution)
+    }
+
+    # If we've gone this far, then the stored inverse matrix is NULL, so we
+    # need to calculate the inverse matrix via the solve() function.
+    data <- x$get()
+    inverse_matrix <- solve(data)
+    x$set_cache(inverse_matrix)
+
+    inverse_matrix
 }
